493a4791afc288980c2d5b4a53ae4267723206bc
Dhiren Brahmbhatt <dhiren.brahmbhattl@saltpay.co>
2022-04-22 09:45:22 +0100

    refactored to switch statement

--- fizzbuzz/fizzbuzz.go
+++ fizzbuzz/fizzbuzz.go
@@ -12,19 +12,17 @@ func Count(num int) string {
 
 	output := []string{}
 	for i := 1; i <= num; i++ {
-		if i%5 == 0 && i%3 == 0 {
+		switch {
+		case i%5 ==0 && i%3 == 0:
 			output = append(output, "fizzbuzz")
 			continue
-		}
-		if i%3 == 0 {
+		case i%3 == 0:
 			output = append(output, "fizz")
 			continue
-		}
-		if i%5 == 0 {
+		case i%5 ==0:
 			output = append(output, "buzz")
 			continue
 		}
-
 		output = append(output, strconv.Itoa(i))
 	}
 	return strings.Join(output, " ")
